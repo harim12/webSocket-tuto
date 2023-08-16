dc06dca896b0dacb3701036d648de5860700a8d2
HASSANI IDRISSI Rim <74383907+harim12@users.noreply.github.com>
2023-08-16 11:35:48 +0100

    project-setup

---
+++ websocket-tuto/src/main/java/com/RimHAASSANI/websockettuto/WebsocketTutoApplication.java
@@ -0,0 +1,13 @@
+package com.RimHAASSANI.websockettuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketTutoApplication.class, args);
+	}
+
+}
---
+++ websocket-tuto/src/test/java/com/RimHAASSANI/websockettuto/WebsocketTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RimHAASSANI.websockettuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
