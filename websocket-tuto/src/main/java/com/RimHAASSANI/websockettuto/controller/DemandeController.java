90f72f673cc787ce65abda986a9be9e3556367ed
HASSANI IDRISSI Rim <74383907+harim12@users.noreply.github.com>
2023-08-16 12:07:09 +0100

    adding web socket configuration

--- websocket-tuto/src/main/java/com/RimHAASSANI/websockettuto/controller/DemandeController.java
+++ websocket-tuto/src/main/java/com/RimHAASSANI/websockettuto/controller/DemandeController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class DemandeController {
 
     @Autowired
